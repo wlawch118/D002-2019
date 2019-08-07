e90a825e1041c5c73c80578975d0123799a2ff1f
wlawch118 <53845642+wlawch118@users.noreply.github.com>
2019-08-07 12:32:29 +0800

    Add files via upload

---
+++ L1/LawCheukHim_Q2.py
@@ -0,0 +1,11 @@
+from math import *
+from math import pi
+print(8*3.57)
+print(5+30*20)
+print((5+30)*20)
+print(1+(2+20*3)/(4*2))
+print(1+2**10)
+print(ceil(29.0/4.0))
+r=(100/pi)**0.5
+A=(2*r+1)**2
+print(A)
