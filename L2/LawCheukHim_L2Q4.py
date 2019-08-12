49548706064b6b72489a99aca08643a1e1704c48
wlawch118 <53845642+wlawch118@users.noreply.github.com>
2019-08-12 10:23:19 +0800

    Add files via upload

--- L2/LawCheukHim_L2Q4.py
+++ L2/LawCheukHim_L2Q4.py
@@ -1,10 +1,7 @@
 ans=0
-a=1
-while a<=57:
-    b=1
-    while b<=(58-a):
-        c=1
-        while c<=(59-a-b):
+for a in range(1,58):
+    for b in range(1,59-a):
+        for c in range(1,60-a-b):
             d=60-a-b-c
             if ans<(a*b+b*c+c*d):
                 ans=(a*b+b*c+c*d)
@@ -12,7 +9,5 @@ while a<=57:
                 ansb=b
                 ansc=c
                 ansd=d
-            c=c+1
-        b=b+1
-    a=a+1
 print(ans,ansa,ansb,ansc,ansd)
+input()
